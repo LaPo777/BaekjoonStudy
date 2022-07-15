c040f3ea5218df50906a4c968784a0e6e51de9a3
Seong Min Lee <lapo7946@gmail.com>
2022-07-15 17:53:12 +0900

    aa

--- src/bronze/bronze1/Main.java
+++ src/bronze/bronze1/Main.java
@@ -1,7 +1,16 @@
 package bronze.bronze1;
-
+/**
+* 작성일 2022-07-15
+*
+*
+*
+*
+*
+**/
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
     }
 }
